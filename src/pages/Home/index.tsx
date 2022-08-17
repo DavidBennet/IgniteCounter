0d85f85c814b2d2a9d7db2d2c83e407be0b6d02f
David Bennet <you@example.com>
2022-08-17 14:22:17 -0300

    adição do zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,57 @@
 import { Play } from 'phosphor-react';
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from './styles';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+import { useState } from 'react';
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe sua tarefa'),
+    minutesAmount: zod
+        .number()
+        .min(5, 'O número não pode ser menor que 5 minutos')
+        .max(60, 'o número não pode passar de 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+    id: string;
+    task: string;
+    minutesAmount: number;
+}
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm()
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
 
-    function handleCreateNewCycle(data: any) {
+    const { register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        }
+    })
 
-    }
-    const task = watch('task')
+    function handleCreateNewCycle(data: NewCycleFormData) {
+        const id = String(new Date().getTime());
+        
+        const newCycle: Cycle = {
+            id, 
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+        }
+
+        setCycles(state => [...state, newCycle])
+        setActiveCycleId(id)
+
+        reset();
+    };
+
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+    const task = watch('task');
+    const isSubmitDisabled = !task;
     
     return (
         <HomeContainer>
