ccb2509834442a8aef35b8a5df1b1098dfea09ac
David Bennet <you@example.com>
2022-08-18 02:40:42 -0300

    correção de bug do contador; lincando o contador ao título da página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -39,12 +39,17 @@ export function Home() {
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
     useEffect(() => {
+        let interval: number;
+
         if (activeCycle) {
-            setInterval(() => {
+            interval = setInterval(() => {
                 setAmountSecondsPassed(
                     differenceInSeconds(new Date(), activeCycle.startDate),
                 )
             }, 1000)
+        }
+        return () => {
+            clearInterval(interval)
         }
     }, [activeCycle])
 
@@ -58,8 +63,9 @@ export function Home() {
             startDate: new Date(),
         }
 
-        setCycles(state => [...state, newCycle])
-        setActiveCycleId(id)
+        setCycles(state => [...state, newCycle]);
+        setActiveCycleId(id);
+        setAmountSecondsPassed(0);
 
         reset();
     };
@@ -72,6 +78,12 @@ export function Home() {
 
     const minutes = String(minutesAmount).padStart(2, '0');
     const seconds = String(secundsAmount).padStart(2, '0');
+
+    useEffect(() => {
+        if (activeCycle) {
+            document.title = `${minutes}:${seconds}`
+        }
+    }, [minutes, seconds, activeCycle]);
 
     const task = watch('task');
     const isSubmitDisabled = !task;
